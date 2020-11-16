7aa5487562a9987abdcdf2937dd523a563d478de
Amine <kurosoul.ae@gmail.com>
2020-11-16 19:10:50 +0100

    update: config token

--- config/panel.php
+++ config/panel.php
@@ -5,34 +5,43 @@
 
 use Illuminate\Support\MessageBag;
 use Illuminate\Support\Facades\Session;
+use Aecodes\AdminPanel\Widgets\Fields\Input;
 
 return [
-	'renderer' => 'blade',
+    'renderer' => 'blade',
 
-	'renderers' => [
-		'json'    => Aecodes\AdminPanel\Responses\JsonRenderer::class,
-		'default' => Aecodes\AdminPanel\Responses\DefaultRenderer::class,
-		'blade'   => Aecodes\LaravelAdminPanel\Responses\BladeRenderer::class,
-	],
+    'renderers' => [
+        'json'    => Aecodes\AdminPanel\Responses\JsonRenderer::class,
+        'default' => Aecodes\AdminPanel\Responses\DefaultRenderer::class,
+        'blade'   => Aecodes\LaravelAdminPanel\Responses\BladeRenderer::class,
+    ],
 
-	'classes' => [
+    'classes' => [
 
-		'link'   => 'inline-flex ml-1 items-center justify-center px-3 py-2 border text-base rounded-md',
+        'link' => 'inline-flex ml-1 items-center justify-center px-3 py-2 border text-base rounded-md',
 
-		'button' => 'inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out',
-		
-	],
+        'button' => 'inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out',
 
-	'menu' => function () {
-		return [];
-	},
+    ],
 
-	'old_value' => function ($name, $default) {
-		return old($name, $default);
-	},
+    'menu' => function () {
+        return [
+            'Pages' => route('pages.index'),
+        ];
+    },
 
-	'errors' => function () {
-		return Session::get('errors', new MessageBag)->all();
-	}
+    'global_fields' => function () {
+        return [
+            Input::hidden('_token')->value(csrf_token())
+        ];
+    },
+
+    'old_value' => function ($name, $default) {
+        return old($name, $default);
+    },
+
+    'errors' => function () {
+        return Session::get('errors', new MessageBag)->all();
+    }
 
 ];
